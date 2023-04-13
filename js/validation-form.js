19066c67ba9b4e7d21370296b91fe9567e3f98d7
joramot <mojica.raul@gmail.com>
2023-04-13 10:04:29 -0600

    UPDATE index.html file and COMPLETE validation-form.js file

--- js/validation-form.js
+++ js/validation-form.js
@@ -1,3 +1,14 @@
 const email = document.getElementById('email');
 const form = document.getElementById('contact_form');
 const small = document.querySelector('small');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  small.innerText = '';
+  if (email.value === email.value.toLowerCase()) {
+    form.submit();
+    return;
+  }
+  small.style.color = '#ff6b00';
+  small.innerText = 'Please enter your email in lowercase.';
+});
